6c76b2ce0a710c5b9a05f61ecea53890dc941d9e
Voltsparx⌁ <voltsparx@gmail.com>
2026-02-18 16:26:07 +0530

    Update output.py

--- core/output.py
+++ core/output.py
@@ -1,1 +1,6 @@
-
+def display(results):
+    print(f"\nScan Results for {results['target']}")
+    print("PORT\tSERVICE")
+    for port, service in results["open_ports"]:
+        print(f"{port}\t{service}")
+    print(f"\nDetected OS: {results['os']}")
